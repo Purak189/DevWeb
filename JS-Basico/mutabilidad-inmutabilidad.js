6303df4e34b80321bf35cd45bacf348d3c37e2a1
Purak189 <106536136+Purak189@users.noreply.github.com>
2023-11-30 13:59:02 -0500

    cambios en el archivo

---
+++ JS-Basico/mutabilidad-inmutabilidad.js
@@ -0,0 +1,13 @@
+//Tipo de dato primitivo - Inmutable
+let numero = 23;
+numero = numero + 10;
+console.log(numero);
+
+let esVerdadero = true;
+esVerdadero = false;
+console.log(esVerdadero);
+
+//Tipo de dato completo - Mutable
+let usuario = {nombre: 'Pepito', edad: 15};
+usuario.edad = 20;
+console.log(usuario);
